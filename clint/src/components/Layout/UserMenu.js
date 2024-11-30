884495627562c7885fba0b8348c35f4ce4add086
mahirlabibx3d <mahirlabib.in@gmail.com>
2024-12-01 01:20:30 +0600

    ad

--- clint/src/components/Layout/UserMenu.js
+++ clint/src/components/Layout/UserMenu.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+
 const UserMenu = () => {
-  return (
-    <div>
-      <div className="text-center dashboard-menu">
-        <div className="list-group">
-          <h4>Dashboard</h4>
-          <NavLink
-            to="/dashboard/user/profile"
-            className="list-group-item list-group-item-action"
-          >
-            Profile
-          </NavLink>
-          <NavLink
-            to="/dashboard/user/orders"
-            className="list-group-item list-group-item-action"
-          >
-            Orders
-          </NavLink>
+    return (
+        <div>
+            <div className="text-center dashboard-menu">
+                <div className="list-group">
+                    <h4>Dashboard</h4>
+                    <NavLink
+                        to="/dashboard/user/profile"
+                        className="list-group-item list-group-item-action"
+                    >
+                        <AiFillHome style={{ marginRight: "8px" }} />
+                        Profile
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/user/orders"
+                        className="list-group-item list-group-item-action"
+                    >
+                        Orders
+                    </NavLink>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default UserMenu;
