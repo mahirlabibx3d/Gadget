f8e9b6df5144ea26b46b827724427deed39d3db4
mahirlabibx3d <mahirlabib.in@gmail.com>
2024-12-01 01:58:00 +0600

    Update search.js

--- clint/src/context/search.js
+++ clint/src/context/search.js
@@ -1,5 +1,7 @@
 import { useState, useContext, createContext } from "react";
+import Layout from "../components/Layout"; // Ensure only this or equivalent is present.
 
+const Layout = () => <div>Some Component</div>;
 const SearchContext = createContext();
 const SearchProvider = ({ children }) => {
   const [auth, setAuth] = useState({
